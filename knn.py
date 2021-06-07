d0442bd16210c809382d68ac43882f845aca7cc1
idardo97 <35769875+idardo97@users.noreply.github.com>
2021-06-07 12:01:28 +0200

    Knn calcule une mauvaise accuracy

--- knn.py
+++ knn.py
@@ -116,12 +116,14 @@ def lsape_populate_instance(first_graph, second_graph, average_node_cost, averag
 
 
 def mapping_from_cost_sans_FW(n, m, g1, g2, node_costs, edge_costs, nodeInsDel, edgeInsDel, ring_g, ring_h):
+    # Finding an adequate mapping based on the given costs, without using the Frank Wolfe method
     c_0 = lsape_populate_instance(g1, g2, node_costs, edge_costs, nodeInsDel,
                                   edgeInsDel, node_costs, nodeInsDel, edge_costs, edgeInsDel, ring_g, ring_h)
     x0 = svd.eps_assigment_from_mapping(torch.exp(-c_0), 10).view((n+1)*(m+1), 1)
     return x0
 
 def new_mapping_from_cost(C,n,m,g1,g2,node_costs,edge_costs,nodeInsDel,edgeInsDel, ring_g, ring_h): 
+    # Finding an adequate mapping based on the given costs, using the Frank Wolfe method
     c=torch.diag(C)       
     c_0 =lsape_populate_instance(g1, g2, node_costs, edge_costs, nodeInsDel,
                                 edgeInsDel, node_costs, nodeInsDel, edge_costs, edgeInsDel, ring_g, ring_h)
@@ -143,6 +145,7 @@ def mapping_from_cost_sans_rings_sans_fw(C, n, m):
     return x0
 
 def mapping_from_cost_method(C,n,m,g1,g2,node_costs,edge_costs,nodeInsDel,edgeInsDel, ring_g, ring_h, rings_andor_fw):
+    # A general function for finding an adequate mapping based on the given costs
     c = torch.diag(C) 
     D = C-torch.eye(C.shape[0],device=device)*c
 
@@ -164,7 +167,7 @@ def mapping_from_cost_method(C,n,m,g1,g2,node_costs,edge_costs,nodeInsDel,edgeIn
 
 
 def primary_ged(k,inputs,node_costs, edge_costs, nodeInsDel, edgeInsDel, rings_andor_fw):
-    # calcul de la ged pour une paire de graphes :
+    # calculation of the ged for a given pair of graphs :
 
     g1 = inputs[k][0]
     g2 = inputs[k][1]
@@ -207,11 +210,9 @@ def primary_ged(k,inputs,node_costs, edge_costs, nodeInsDel, edgeInsDel, rings_a
 
 
 def gedd(data, node_costs, edge_costs, nodeInsDel, edgeInsDel, rings_andor_fw):
-    # calcul de la ged pour toutes les paires de graphes :
-
+    # calculation of the ged for all pairs of graphs :
     inputs = data.to(device)
     ged = torch.zeros(len(inputs)).to(device)
-
     for k in range(len(inputs)):
         ged[k] = primary_ged(k,inputs,node_costs, edge_costs, nodeInsDel, edgeInsDel, rings_andor_fw)
 
@@ -244,6 +245,7 @@ def ged_from_pkl(rings_andor_fw):
     return ged_pkl  #new_ged_pkl
 
 def train_data(train_size,class1, class2):
+    # Splitting data
     nb_elt = int(train_size*(train_size-1)/2)
     data = torch.empty((nb_elt, 2), dtype=torch.int)
     train_graphs = torch.cat((class1, class2), 0)
@@ -255,6 +257,7 @@ def train_data(train_size,class1, class2):
     return data,train_graphs
 
 def new_class(class1,random_class1):
+    #Creating a new class different from the given class 
     tmp=class1.numpy()
     res=[]
     for i in range(len(tmp)-1):
@@ -267,22 +270,26 @@ def new_class(class1,random_class1):
 def tr_test_data(train_size, class1, class2):
     nb_class1=12
     nb_class2=int((nb_class1-1)/2)
-    nb_elt=int(nb_class1*(nb_class1+2*nb_class2-1)/2)
+    #nb_elt=int(nb_class1*(nb_class1+2*nb_class2-1)/2)
+    nb_elt=56
+    print("nb_elt = ",nb_elt)
     #nb_elt = int(train_size*(train_size-1)/2)
+    
     data = torch.empty((nb_elt, 2), dtype=torch.int)
     yt=torch.ones(nb_elt)
-    random_class1=class1[(torch.abs(10000*torch.randn(nb_class1)).int()%class1.size()[0]).long()]
-    random_class2=class2[(torch.abs(10000*torch.randn(nb_class2)).int()%class2.size()[0]).long()]
+    random_class1=class1[(torch.abs(10000*torch.randn(30)).int()%class1.size()[0]).long()]
+    random_class2=class2[(torch.abs(10000*torch.randn(26)).int()%class2.size()[0]).long()]
     train_graphs=torch.cat((random_class1,random_class2),0)
 
-    couples=torch.triu_indices(train_size,train_size,offset=1)
+    couples=torch.triu_indices(56,56,offset=1)  #(train_size,train_size,offset=1)
     data[0:nb_elt,0]=train_graphs[couples[0,0:nb_elt]]
     data[0:nb_elt,1]=train_graphs[couples[1,0:nb_elt]]
     for k in range(nb_elt):
         if (y[data[k][0]]!=y[data[k][1]]):
             yt[k]=-1.0      
-    [train_D, valid_D,train_L,valid_L]= train_test_split(data,yt, test_size=0.25,train_size=0.75, shuffle=True) #, stratify=yt)
-        
+    #[train_D, valid_D,train_L,valid_L]= train_test_split(data,yt, test_size=0.25,train_size=0.75, shuffle=True) #, stratify=yt)
+    [train_D, valid_D,train_L,valid_L]= train_test_split(data,yt, test_size=0.25,train_size=0.75, shuffle=True, stratify=yt) 
+
     #DatasetTrain = TensorDataset(train_D, train_L)
     #DatasetValid=TensorDataset(valid_D, valid_L)
 
@@ -293,63 +300,134 @@ def tr_test_data(train_size, class1, class2):
     print("len data = ",len(data),data,'\n')
     return data,train_graphs,train_D,valid_D,train_L,valid_L
 
+def new_primary_ged(g1,g2,n,m,node_costs, edge_costs, nodeInsDel, edgeInsDel, rings_andor_fw):
+    # calculation of the ged for a given pair of graphs :
 
-def train_test_data(train_size, test_size, class1, class2):
-    nb_elt = int(train_size*(train_size-1)/2)
-    data = torch.empty((nb_elt, 2), dtype=torch.int)
-
-    if train_size % 2 == 0:
-        nb_class1=int(train_size/2)
-        nb_class2=int(train_size/2)
-    else:
-        nb_class1=int(train_size/2)+1
-        nb_class2=int(train_size/2)
-        
-    if test_size % 2 == 0:
-        nb_class1_test=int(test_size/2)
-        nb_class2_test=int(test_size/2)
-    else:
-        nb_class1_test=int(test_size/2)+1
-        nb_class2_test=int(test_size/2)
-        
-    #print((torch.abs(10000*torch.randn(nb_class1)).int()%class1.size()[0]).long())
-    random_class1=class1[(torch.abs(10000*torch.randn(nb_class1)).int()%class1.size()[0]).long()]
-    random_class2=class2[(torch.abs(10000*torch.randn(nb_class2)).int()%class2.size()[0]).long()]
+    if (rings_andor_fw=='rings_sans_fw'):
+        ring_g,ring_h = rings.build_rings(g1,edgeInsDel.size()), rings.build_rings(g2,edgeInsDel.size())
+        C = construct_cost_matrix(g1, g2, node_costs, edge_costs, nodeInsDel, edgeInsDel)
+        S = mapping_from_cost_method(C,n,m,g1,g2,node_costs,edge_costs,nodeInsDel,edgeInsDel, ring_g, ring_h, rings_andor_fw)
+        #S=mapping_from_cost_sans_FW(n,m,g1,g2,node_costs,edge_costs,nodeInsDel,edgeInsDel,ring_g,ring_h)
     
-    class1=new_class(class1,random_class1)
-    class2=new_class(class2,random_class2)
+    if (rings_andor_fw=='rings_avec_fw'):
+        ring_g,ring_h = rings.build_rings(g1,edgeInsDel.size()), rings.build_rings(g2,edgeInsDel.size())
+        C = construct_cost_matrix(g1, g2, node_costs, edge_costs, nodeInsDel, edgeInsDel)
+        S = mapping_from_cost_method(C,n,m,g1,g2,node_costs,edge_costs,nodeInsDel,edgeInsDel, ring_g, ring_h, rings_andor_fw)
+        #S = new_mapping_from_cost(C,n,m,g1,g2,node_costs,edge_costs,nodeInsDel,edgeInsDel, ring_g, ring_h)
+    if (rings_andor_fw=='sans_rings_avec_fw'):
+        C = construct_cost_matrix(g1, g2, node_costs, edge_costs, nodeInsDel, edgeInsDel)
+        S = mapping_from_cost_method(C,n,m,g1,g2,node_costs,edge_costs,nodeInsDel,edgeInsDel, 0, 0, rings_andor_fw)
+        #S = mapping_from_cost(C, n, m)
+    if (rings_andor_fw=='sans_rings_sans_fw'):
+        C = construct_cost_matrix(g1, g2, node_costs, edge_costs, nodeInsDel, edgeInsDel)
+        S = mapping_from_cost_method(C,n,m,g1,g2,node_costs,edge_costs,nodeInsDel,edgeInsDel, 0, 0, rings_andor_fw)
+        #S = mapping_from_cost_sans_rings_sans_fw(C,n,m)
 
-    random_class1_test=class1[(torch.abs(10000*torch.randn(nb_class1_test)).int()%class1.size()[0]).long()]
-    random_class2_test=class2[(torch.abs(10000*torch.randn(nb_class2_test)).int()%class2.size()[0]).long()]
+    v = torch.flatten(S)
 
-    train_graphs=torch.cat((random_class1,random_class2),0)
-    test_graphs=torch.cat((random_class1_test,random_class2_test),0)
+    normalize_factor = 1.0
+    if normalize:
+        nb_edge1 = (A[g1][0:n*n] != torch.zeros(n *n, device=device)).int().sum()
+        nb_edge2 = (A[g2][0:m*m] != torch.zeros(m *m, device=device)).int().sum()
+        normalize_factor = nodeInsDel*(n+m)+edgeInsDel*(nb_edge1+nb_edge2)
+    c = torch.diag(C)
+    D = C-torch.eye(C.shape[0], device=device)*c
+    ged_k = (.5*v.t()@D@v+c.T@v)/normalize_factor
 
-    couples = torch.triu_indices(train_size, train_size, offset=1)
-    data[0:nb_elt, 0] = train_graphs[couples[0, :]]
-    data[0:nb_elt, 1] = train_graphs[couples[1, :]]
+    return ged_k
 
-    couples_test=torch.triu_indices(test_size,train_size,offset=1)
-    print('couples_test size : ',couples_test.size())
-    nb_elt_test= int((test_size)*(train_size-4))
-    print('nb_elt_test = ',nb_elt_test)
-    data_test=torch.empty((nb_elt_test,2),dtype=torch.int)
-    data_test[0:nb_elt_test,0]=test_graphs[couples_test[0,:]]
-    data_test[0:nb_elt_test,1]=test_graphs[couples_test[1,:]]
-    print('data_test : ', data_test.size())
-    print('test_graphs : ', test_graphs.size())
+
+def calculates_distances(rings_andor_fw, node_costs, edge_costs, nodeInsDel, edgeInsDel,train_D, valid_D,train_L,valid_L): #returns two matrices of distances (train and test)
+    # Calculating the geds of the couples of graphs in the train and the test data, given a previous splitting of the data
     
-    print('data_test : ',data_test)
-    print('train_graphs : ', train_graphs)
-    return data, train_graphs, data_test, test_graphs
+    my_list=[i for i in range(68)] 
+    #[train_D, valid_D,train_L,valid_L]= train_test_split(my_list,y[:8], test_size=0.25,train_size=0.75, shuffle=True) 
+    n_train=len(train_L)
+    n_valid=len(valid_L)
+
+    # Matrices of distances for train and test
+    D_train=np.zeros((n_train,n_train))
+    D_valid=np.zeros((n_valid,n_train))
+
+    # Calculate ged between couples of graphs (train graphs*train graphs)
+    for i,g1_idx in enumerate(train_D): #:51
+        print(i)
+        for j,g2_idx in enumerate(train_D):
+            print(j,g2_idx)
+            n=Gs[g1_idx].order()
+            m=Gs[g2_idx].order()
+            '''
+            ring_g,ring_h = rings.build_rings(g1_idx,edgeInsDel.size()), rings.build_rings(g2_idx,edgeInsDel.size())
+            C = construct_cost_matrix(g1_idx, g2_idx, node_costs, edge_costs, nodeInsDel, edgeInsDel)
+            S = mapping_from_cost_method(C,n,m,g1_idx,g2_idx,node_costs,edge_costs,nodeInsDel,edgeInsDel, ring_g, ring_h, rings_andor_fw)
+            v = torch.flatten(S)
+
+            normalize_factor = 1.0
+            c = torch.diag(C)
+            D = C-torch.eye(C.shape[0], device=device)*c
+            ged_k = (.5*v.t()@D@v+c.T@v)/normalize_factor
+            '''
+            ged_k=new_primary_ged(g1_idx,g2_idx,n,m,node_costs, edge_costs, nodeInsDel, edgeInsDel, rings_andor_fw)
+            # Filling the corresponding matrix of ged D_train :
+            D_train[i,j]=ged_k
+
+    print("now test : ")
+    # Calculate ged between couples of graphs (test graphs*train graphs)
+    for i,g1_idx in enumerate(valid_D):
+        print(i,g1_idx)
+        for j,g2_idx in enumerate(train_D):
+            print(j,g2_idx)
+            n=Gs[g1_idx].order()
+            m=Gs[g2_idx].order()
+            '''
+            ring_g,ring_h = rings.build_rings(g1_idx,edgeInsDel.size()), rings.build_rings(g2_idx,edgeInsDel.size())
+            C = construct_cost_matrix(g1_idx, g2_idx, node_costs, edge_costs, nodeInsDel, edgeInsDel)
+            S = mapping_from_cost_method(C,n,m,g1_idx,g2_idx,node_costs,edge_costs,nodeInsDel,edgeInsDel, ring_g, ring_h, rings_andor_fw)
+            v = torch.flatten(S)
+
+            normalize_factor = 1.0
+            c = torch.diag(C)
+            D = C-torch.eye(C.shape[0], device=device)*c
+            ged_k = (.5*v.t()@D@v+c.T@v)/normalize_factor
+            '''
+            # Filling the corresponding matrix of ged D_valid :
+            ged_k=new_primary_ged(g1_idx,g2_idx,n,m,node_costs, edge_costs, nodeInsDel, edgeInsDel, rings_andor_fw)
+            D_valid[i,j]=ged_k
+            
+    print("D_valid.shape = ", D_valid.shape)
+    return D_train,D_valid,train_L,valid_L 
+
+def knn_ines(D_train,D_valid,train_L,valid_L): 
+    # a knn classifier to evaluate the performance of our model
+     
+    classifier = KNeighborsClassifier(n_neighbors=3, metric='precomputed')
+    print(len(train_L))
+    classifier.fit(D_train, train_L)  # train_size*train_size
+    y_pred_train = classifier.predict(D_train)
+    y_pred_valid = classifier.predict(D_valid)
+    print('y_pred_train : ', y_pred_train)
+    print('y_pred_valid : ', y_pred_valid)
+    #print(confusion_matrix(y_test, y_pred))
+    #print('acc : ', np.mean(y_pred == y)*100)
+    print("valid_L = ",valid_L, len(valid_L))
+    print(classification_report(valid_L, y_pred_valid)) 
+    print(classification_report(train_L, y_pred_train))
 
 
 def knn(train_graphs, train_size, ged_pkl,train_D,valid_D,train_L,valid_L):
-    
-    triu_indices = torch.triu_indices(row=train_size, col=train_size, offset=1)
-    D = torch.zeros((train_size, train_size))
-    D[triu_indices[0, :], triu_indices[1, :]][:126] = ged_pkl
+    new_train_size = 56
+    triu_indices = torch.triu_indices(row=new_train_size, col=new_train_size, offset=1)
+    D = torch.zeros((new_train_size, new_train_size))
+    D[triu_indices[0, :], triu_indices[1, :]][:56] = ged_pkl
     D = D+D.t()
+    test_size=int((new_train_size*25)/100)
+    D_test = torch.zeros((test_size, new_train_size))
+
+
+    #triu_indices_test = torch.triu_indices(row=test_size, col=new_train_size, offset=1)
+    #D_test[triu_indices_test[0, :], triu_indices_test[1, :]] = ged_pkl
+    #D_test = D_test + D_test.t()
+
     '''
     plt.matshow(D)
     plt.colorbar()
@@ -357,13 +435,14 @@ def knn(train_graphs, train_size, ged_pkl,train_D,valid_D,train_L,valid_L):
     plt.show()
     '''
     classifier = KNeighborsClassifier(n_neighbors=3, metric='precomputed')
-    print('D.shape : ',D.shape)
+    print('D_test.shape : ',D_test.shape)
 
     ytrain = []
     for g in train_graphs:
         ytrain.append(y[g])
     print('ytrain : ', ytrain)
     print('train_size = ', train_size)
+    print("new_train_size = ",new_train_size)
     
     # plt.plot(ytrain)
     # plt.show()
@@ -372,12 +451,12 @@ def knn(train_graphs, train_size, ged_pkl,train_D,valid_D,train_L,valid_L):
     print("train_L = ",len(train_L))
     classifier.fit(D, ytrain)  # train_size*train_size
     #y_pred = classifier.predict(D)  # test_size*train_size
-    y_pred = classifier.predict(D) #valid_D
+    y_pred = classifier.predict(D_test) #valid_D
     print('y_pred : ', y_pred)
     #print(confusion_matrix(y_test, y_pred))
     print('acc : ', np.mean(y_pred == y)*100)
     print("valid_L = ",valid_L, len(valid_L))
-    print(classification_report(valid_L[:17], y_pred)) #valid_L,y_pred
+    print(classification_report(valid_L, y_pred)) #valid_L,y_pred
     #print(classification_report(data_test, y_pred)) #add data_test in parameters
 
     plt.plot(D[12, :], label='D[12, :]')
@@ -391,7 +470,6 @@ def main_testing_function(train_size, rings_andor_fw, node_costs, edge_costs, no
     data,train_graphs,train_D,valid_D,train_L,valid_L = tr_test_data(train_size, class1, class2)
     ged_pkl=ged_to_pkl(data, node_costs, edge_costs, nodeInsDel, edgeInsDel, rings_andor_fw)
 
-    
     g1=0
     g2=1
     C=construct_cost_matrix(g1,g2, node_costs, edge_costs, nodeInsDel, edgeInsDel)
@@ -399,7 +477,7 @@ def main_testing_function(train_size, rings_andor_fw, node_costs, edge_costs, no
     plt.matshow(C)
     plt.title('Cost matrix for g1=0 and g2=1')
     plt.show()
-    
+
     new_ged_pkl = ged_from_pkl(rings_andor_fw)
 
     knn(train_graphs, train_size, new_ged_pkl, train_D,valid_D,train_L,valid_L)
@@ -421,39 +499,50 @@ if __name__ == "__main__":
     class1 = torch.tensor([k for k in range(len(y)) if y[k] == 1])
     class2 = torch.tensor([k for k in range(len(y)) if y[k] == 0])
 
-    new_node_costs = torch.load('nodeSub_min',pickle_module=pkl)
-    new_nodeInsDel = torch.load('nodeInsDel_min',pickle_module=pkl)
-    new_edgeInsDel = torch.load('edgeInsDel_min',pickle_module=pkl)
-    new_edge_costs = torch.load('edgeSub_min',pickle_module=pkl)
+    new_node_costs = torch.load('../Luc/deepged/nodeSub_min',pickle_module=pkl)
+    new_nodeInsDel = torch.load('../Luc/deepged/nodeInsDel_min',pickle_module=pkl)
+    new_edgeInsDel = torch.load('../Luc/deepged/edgeInsDel_min',pickle_module=pkl)
+    new_edge_costs = torch.load('../Luc/deepged/edgeSub_min',pickle_module=pkl)
     new_node_costs.requires_grad=False
     new_nodeInsDel.requires_grad=False
     new_edgeInsDel.requires_grad=False
     new_edge_costs.requires_grad=False
 
-    '''
-
-    node_costs_2 = torch.ones(node_costs.shape)
+    
+    node_costs_2 = torch.ones(new_node_costs.shape)
     for i in range(node_costs_2.shape[0]):
         node_costs_2[i,i] = 0
 
-    edge_costs_2 = torch.ones(edge_costs.shape)
+    edge_costs_2 = torch.ones(new_edge_costs.shape)
     for i in range(edge_costs_2.shape[0]):
         edge_costs_2[i,i] = 0
 
     nodeInsDel_2 = torch.tensor(3.0)
     edgeInsDel_2 = torch.tensor(3.0)
-    '''
+    
 
     rings_andor_fw = 'rings_sans_fw'  #'sans_rings_sans_fw' #sans_rings_avec_fw
     #train_size = 68
     nb_class1=12
     nb_class2=int((nb_class1-1)/2)
     train_size=nb_class1+nb_class2
-    main_testing_function(train_size, rings_andor_fw, new_node_costs, new_edge_costs, new_nodeInsDel, new_edgeInsDel, class1, class2)
+    #train_size=51
+    #main_testing_function(train_size, rings_andor_fw, new_node_costs, new_edge_costs, new_nodeInsDel, new_edgeInsDel, class1, class2)
 
+    train_D = torch.load('../Luc/deepged/train_D',pickle_module=pkl) 
+    valid_D = torch.load('../Luc/deepged/valid_D',pickle_module=pkl) 
+    train_L = torch.load('../Luc/deepged/train_L',pickle_module=pkl) 
+    valid_L = torch.load('../Luc/deepged/valid_L',pickle_module=pkl) 
 
+    print("train_D = ",train_D, len(train_D))
+    print("train_L = ",train_L)
+    print("valid_D = ",valid_D, len(valid_D))
+    print("valid_L = ",valid_L)
     
-   
+    #D_train,D_valid,train_L,valid_L=calculates_distances(rings_andor_fw, new_node_costs, new_edge_costs, new_nodeInsDel, new_edgeInsDel,train_D, valid_D,train_L,valid_L)
+    D_train,D_valid,train_L,valid_L=calculates_distances(rings_andor_fw, node_costs_2, edge_costs_2, nodeInsDel_2, edgeInsDel_2,train_D, valid_D,train_L,valid_L)
+    print(D_train,len(D_train),len(D_train[0]))
+    knn_ines(D_train,D_valid,train_L,valid_L)
 
 
 
