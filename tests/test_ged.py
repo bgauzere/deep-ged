17b6fca9fb46051531932d3adca25ecd512d5bbc
Gauzere Benoit <benoit.gauzere@insa-rouen.fr>
2021-11-17 22:08:58 +0100

    Un test unitaire sur la ged + pipfile

---
+++ tests/test_ged.py
@@ -0,0 +1,33 @@
+from data_manager.label_manager import compute_extended_labels
+import pytest
+from layers.layer import Net
+from gklearn.utils.graphfiles import loadDataset
+import torch
+from graph_torch import ged_torch
+
+Gs, y = loadDataset('/home/bgauzere/work/Recherche/Datasets/MAO/dataset.ds')
+for g in Gs:
+    compute_extended_labels(g)
+
+model = Net(Gs)
+
+
+@pytest.fixture(scope="function")
+def a_graph():
+    return 12
+
+
+def test_ged_0(a_graph):
+    """
+    test si la ged d'un graphe avec lui meme est 0
+    """
+    cns, cndl, ces, cedl = model.from_weighs_to_costs()
+    n = model.card[a_graph]
+    C = model.construct_cost_matrix(a_graph, a_graph, cns, ces, cndl, cedl)
+    v = torch.flatten(torch.eye(n+1))
+
+    c = torch.diag(C)
+    D = C - torch.eye(C.shape[0], ) * c
+    ged = (.5 * v.T @ D @ v + c.T @ v)
+
+    assert (ged == 0)
