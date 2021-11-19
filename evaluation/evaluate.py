23945702eea6eaaa16a0ccc463bd17c9518f44a9
Gauzere Benoit <benoit.gauzere@insa-rouen.fr>
2021-11-19 15:59:44 +0100

    Correction bug index graphes

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -1,3 +1,15 @@
+import matplotlib
+import networkx as nx
+import sys
+from gklearn.utils.graphfiles import loadDataset
+from data_manager.label_manager import *
+import pickle as pkl
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import numpy as np
+import rings
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -6,22 +18,8 @@ from svd import iterated_power as compute_major_axis
 import GPUtil
 torch.backends.cudnn.benchmark = True
 torch.backends.cudnn.enabled = True
-#torch.autograd.set_detect_anomaly(True)
-import rings
-import numpy as np
-from tqdm import tqdm
-import matplotlib.pyplot as plt
-from sklearn.metrics import classification_report, confusion_matrix
-from sklearn.neighbors import KNeighborsClassifier
-import pickle as pkl
-from data_manager.label_manager import *
-from gklearn.utils.graphfiles import loadDataset
-import sys
-import networkx as nx
-import matplotlib
+# torch.autograd.set_detect_anomaly(True)
 matplotlib.use('TkAgg')
-
-
 
 
 class Evaluator():
@@ -33,10 +31,13 @@ class Evaluator():
         print(self.nb_edge_labels)
         # self.device=torch.device("cuda:0")
         self.device = torch.device('cpu')
-        self.card = torch.tensor([G.order() for G in GraphList]).to(self.device)
+        self.card = torch.tensor([G.order()
+                                 for G in GraphList]).to(self.device)
         card_max = self.card.max()
-        self.A = torch.empty((len(GraphList), card_max * card_max), dtype=torch.int, device=self.device)
-        self.labels = torch.empty((len(GraphList), card_max), dtype=torch.int, device=self.device)
+        self.A = torch.empty(
+            (len(GraphList), card_max * card_max), dtype=torch.int, device=self.device)
+        self.labels = torch.empty(
+            (len(GraphList), card_max), dtype=torch.int, device=self.device)
         print(self.A.shape)
         for k in range(len(GraphList)):
             A, l = self.from_networkx_to_tensor(GraphList[k], dict)
@@ -48,13 +49,15 @@ class Evaluator():
 
     def classification(self, train_data, test_data, train_label, test_label, k, cns, ces, cndl, cedl):
 
-        n_graph_test = len(test_graph)
-        n_graph_train = len(train_graph)
+        n_graph_test = len(test_data)
+        n_graph_train = len(train_data)
         ged_matrix_train = np.zeros((n_graph_train, n_graph_train))
         ged_matrix_test = np.zeros((n_graph_test, n_graph_train))
 
-        for g1 in tqdm(range(len(train_data))):
-            for g2 in range(len(train_data)):
+        for i in tqdm(range(len(train_data))):
+            g1 = train_data[i]
+            for j in range(len(train_data)):
+                g2 = train_data[j]
                 n = self.card[g1]
                 m = self.card[g2]
                 # with torch.no_grad():
@@ -70,16 +73,21 @@ class Evaluator():
 
                 normalize_factor = 1.0
                 if self.normalize:
-                    nb_edge1 = (self.A[g1][0:n * n] != torch.zeros(n * n)).int().sum()
-                    nb_edge2 = (self.A[g2][0:m * m] != torch.zeros(m * m)).int().sum()
-                    normalize_factor = cndl * (n + m) + cedl * (nb_edge1 + nb_edge2)
+                    nb_edge1 = (self.A[g1][0:n * n] !=
+                                torch.zeros(n * n)).int().sum()
+                    nb_edge2 = (self.A[g2][0:m * m] !=
+                                torch.zeros(m * m)).int().sum()
+                    normalize_factor = cndl * \
+                        (n + m) + cedl * (nb_edge1 + nb_edge2)
                 c = torch.diag(C)
                 D = C - torch.eye(C.shape[0], device=self.device) * c
                 ged = (.5 * v.T @ D @ v + c.T @ v) / normalize_factor
-                ged_matrix_train[g1, g2] = ged
+                ged_matrix_train[i, j] = ged
 
-        for g1 in tqdm(range(len(test_data))):
-            for g2 in range(len(train_data)):
+        for i in tqdm(range(len(test_data))):
+            g1 = test_data[i]
+            for j in range(len(train_data)):
+                g2 = train_data[j]
                 n = self.card[g1]
                 m = self.card[g2]
                 # with torch.no_grad():
@@ -95,13 +103,16 @@ class Evaluator():
 
                 normalize_factor = 1.0
                 if self.normalize:
-                    nb_edge1 = (self.A[g1][0:n * n] != torch.zeros(n * n)).int().sum()
-                    nb_edge2 = (self.A[g2][0:m * m] != torch.zeros(m * m)).int().sum()
-                    normalize_factor = cndl * (n + m) + cedl * (nb_edge1 + nb_edge2)
+                    nb_edge1 = (self.A[g1][0:n * n] !=
+                                torch.zeros(n * n)).int().sum()
+                    nb_edge2 = (self.A[g2][0:m * m] !=
+                                torch.zeros(m * m)).int().sum()
+                    normalize_factor = cndl * \
+                        (n + m) + cedl * (nb_edge1 + nb_edge2)
                 c = torch.diag(C)
                 D = C - torch.eye(C.shape[0], device=self.device) * c
                 ged = (.5 * v.T @ D @ v + c.T @ v) / normalize_factor
-                ged_matrix_test[g1, g2] = ged
+                ged_matrix_test[i, j] = ged
 
         # Definition of the KNN classifier. precomputed stands for "precomputed distances", which we did by computing GED
         classifier = KNeighborsClassifier(n_neighbors=k, metric="precomputed")
@@ -117,7 +128,7 @@ class Evaluator():
         plt.subplot(122)
         plt.imshow(ged_matrix_test)
         plt.show()
-
+        return ged_matrix_train, ged_matrix_test
     # def forward(self, input):
     #     ged = torch.zeros(len(input)).to(self.device)
     #     node_costs, nodeInsDel, edge_costs, edgeInsDel = self.from_weighs_to_costs()
@@ -174,14 +185,18 @@ class Evaluator():
         # ce=ce/total_cost
         edgeInsDel = ce[-1]
 
-        node_costs = torch.zeros((self.nb_labels, self.nb_labels), device=self.device)
-        upper_part = torch.triu_indices(node_costs.shape[0], node_costs.shape[1], offset=1, device=self.device)
+        node_costs = torch.zeros(
+            (self.nb_labels, self.nb_labels), device=self.device)
+        upper_part = torch.triu_indices(
+            node_costs.shape[0], node_costs.shape[1], offset=1, device=self.device)
         node_costs[upper_part[0], upper_part[1]] = cn[0:-1]
         node_costs = node_costs + node_costs.T
 
         if self.nb_edge_labels > 1:
-            edge_costs = torch.zeros((self.nb_edge_labels, self.nb_edge_labels), device=self.device)
-            upper_part = torch.triu_indices(edge_costs.shape[0], edge_costs.shape[1], offset=1, device=self.device)
+            edge_costs = torch.zeros(
+                (self.nb_edge_labels, self.nb_edge_labels), device=self.device)
+            upper_part = torch.triu_indices(
+                edge_costs.shape[0], edge_costs.shape[1], offset=1, device=self.device)
             edge_costs[upper_part[0], upper_part[1]] = ce[0:-1]
             edge_costs = edge_costs + edge_costs.T
         else:
@@ -209,7 +224,8 @@ class Evaluator():
         return dict, max(max([[int(G[e[0]][e[1]]['bond_type']) for e in G.edges()] for G in GraphList]))
 
     def from_networkx_to_tensor(self, G, dict):
-        A = torch.tensor(nx.to_scipy_sparse_matrix(G, dtype=int, weight='bond_type').todense(), dtype=torch.int)
+        A = torch.tensor(nx.to_scipy_sparse_matrix(
+            G, dtype=int, weight='bond_type').todense(), dtype=torch.int)
         lab = [dict[G.nodes[v][self.node_label][0]] for v in nx.nodes(G)]
 
         return (A.view(1, A.shape[0] * A.shape[1]), torch.tensor(lab))
@@ -218,9 +234,11 @@ class Evaluator():
         n = self.card[g1].item()
         m = self.card[g2].item()
         with torch.no_grad():
-            A1 = torch.zeros((n + 1, n + 1), dtype=torch.int, device=self.device)
+            A1 = torch.zeros((n + 1, n + 1), dtype=torch.int,
+                             device=self.device)
             A1[0:n, 0:n] = self.A[g1][0:n * n].view(n, n)
-            A2 = torch.zeros((m + 1, m + 1), dtype=torch.int, device=self.device)
+            A2 = torch.zeros((m + 1, m + 1), dtype=torch.int,
+                             device=self.device)
             A2[0:m, 0:m] = self.A[g2][0:m * m].view(m, m)
             A = self.matrix_edgeInsDel(A1, A2)
 
@@ -233,7 +251,8 @@ class Evaluator():
             for k in range(self.nb_edge_labels):
                 for l in range(self.nb_edge_labels):
                     if k != l:
-                        C.add_(self.matrix_edgeSubst(A1, A2, k + 1, l + 1).multiply_(edge_costs[k][l]))
+                        C.add_(self.matrix_edgeSubst(A1, A2, k + 1,
+                               l + 1).multiply_(edge_costs[k][l]))
 
         # C=cost[3]*torch.tensor(np.array([ [  k!=l and A1[k//(m+1),l//(m+1)]^A2[k%(m+1),l%(m+1)] for k in range((n+1)*(m+1))] for l in range((n+1)*(m+1))]),device=self.device)
 
@@ -245,7 +264,8 @@ class Evaluator():
         D[[i * (m + 1) + m for i in range(n)]] = nodeInsDel
         for k in range(n * (m + 1)):
             if k % (m + 1) != m:
-                D[k] = node_costs[l1[k // (m + 1)]][l2[k % (m + 1)]]  # self.get_node_costs(l1[k//(m+1)],l2[k%(m+1)])
+                # self.get_node_costs(l1[k//(m+1)],l2[k%(m+1)])
+                D[k] = node_costs[l1[k // (m + 1)]][l2[k % (m + 1)]]
 
         # D[[k for k in range(n*(m+1)) if k%(m+1) != m]]=torch.tensor([node_costs[l1[k//(m+1)],l2[k%(m+1)]] for k in range(n*(m+1)) if k%(m+1) != m],device=self.device )
         with torch.no_grad():
@@ -257,8 +277,10 @@ class Evaluator():
         return C
 
     def matrix_edgeInsDel(self, A1, A2):
-        Abin1 = (A1 != torch.zeros((A1.shape[0], A1.shape[1]), device=self.device))
-        Abin2 = (A2 != torch.zeros((A2.shape[0], A2.shape[1]), device=self.device))
+        Abin1 = (A1 != torch.zeros(
+            (A1.shape[0], A1.shape[1]), device=self.device))
+        Abin2 = (A2 != torch.zeros(
+            (A2.shape[0], A2.shape[1]), device=self.device))
         C1 = torch.einsum('ij,kl->ijkl', torch.logical_not(Abin1), Abin2)
         C2 = torch.einsum('ij,kl->ijkl', Abin1, torch.logical_not(Abin2))
         C12 = torch.logical_or(C1, C2).int()
@@ -266,8 +288,10 @@ class Evaluator():
         return torch.cat(torch.unbind(torch.cat(torch.unbind(C12, 1), 1), 0), 1)
 
     def matrix_edgeSubst(self, A1, A2, lab1, lab2):
-        Abin1 = (A1 == lab1 * torch.ones((A1.shape[0], A1.shape[1]), device=self.device)).int()
-        Abin2 = (A2 == lab2 * torch.ones((A2.shape[0], A2.shape[1]), device=self.device)).int()
+        Abin1 = (
+            A1 == lab1 * torch.ones((A1.shape[0], A1.shape[1]), device=self.device)).int()
+        Abin2 = (
+            A2 == lab2 * torch.ones((A2.shape[0], A2.shape[1]), device=self.device)).int()
         C = torch.einsum('ij,kl->ijkl', Abin1, Abin2)
 
         return torch.cat(torch.unbind(torch.cat(torch.unbind(C, 1), 1), 0), 1).float()
@@ -286,7 +310,8 @@ class Evaluator():
 
         node_costs, nodeInsDel, edge_costs, edgeInsDel = self.from_weighs_to_costs()
 
-        lsape_instance = [[0 for _ in range(len(g) + 1)] for __ in range(len(h) + 1)]
+        lsape_instance = [[0 for _ in range(len(g) + 1)]
+                          for __ in range(len(h) + 1)]
         for g_node_index in range(len(g) + 1):
             for h_node_index in range(len(h) + 1):
                 lsape_instance[h_node_index][g_node_index] = rings.compute_ring_distance(g, h, self.ring_g, self.ring_h,
@@ -304,7 +329,8 @@ class Evaluator():
     def mapping_from_cost(self, C, n, m):
         c = torch.diag(C)
         D = C - torch.eye(C.shape[0], device=self.device) * c
-        x0 = svd.eps_assign2(torch.exp(-.5 * c.view(n + 1, m + 1)), 10).view((n + 1) * (m + 1), 1)
+        x0 = svd.eps_assign2(
+            torch.exp(-.5 * c.view(n + 1, m + 1)), 10).view((n + 1) * (m + 1), 1)
 
         x = svd.franck_wolfe(x0, D, c, 5, 10, n, m)
 
@@ -321,7 +347,8 @@ class Evaluator():
         # first_ev=self.iterated_power(M,inv=True)
         if (first_ev.sum() < 0):
             first_ev = -first_ev
-        S = torch.exp(first_ev.view(n + 1, m + 1))  # enforce the difference, accelerate the convergence.
+        # enforce the difference, accelerate the convergence.
+        S = torch.exp(first_ev.view(n + 1, m + 1))
         S = self.eps_assigment_from_mapping(S)
         return S
 
@@ -350,7 +377,6 @@ if __name__ == "__main__":
     params = {'batch_size': batch_size,
               'shuffle': True,
               'num_workers': 0}
-
 
     rings_andor_fw = "sans_rings_avec_fw"
     weights = "learned"
