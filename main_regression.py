bb978ca3daa822b09732a029ace2650853130aed
Sidney Besnard <21707677@etu.unicaen.fr>
2022-01-19 14:45:00 +0100

    Ajout code regression

---
+++ main_regression.py
@@ -0,0 +1,154 @@
+import os
+import sys
+import pickle as pkl
+import torch
+import GPUtil
+import matplotlib.pyplot as plt
+import matplotlib
+import argparse
+
+from gklearn.utils.graphfiles import loadDataset
+from sklearn.model_selection import train_test_split
+
+
+from deepged.learning import GEDclassification
+from deepged.data_manager.label_manager import compute_extended_labels, build_node_dictionnary
+from deepged.model import GedLayer
+from deepged.utils import from_networkx_to_tensor
+from model_regression import RegressGedLayer
+
+
+matplotlib.use('TkAgg')
+
+
+
+if __name__ == "__main__":
+    dico_device = {"cpu": 'cpu', 'gpu': 'cuda:0'}
+    dico_calc = {0: 'rings_sans_fw', 1: 'sans_rings_avec_fw',
+                 2: 'rings_avec_fw', 3: 'sans_rings_sans_fw'}
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-v", '--verbosity', help="Print differents informations on the model", action="store_true")
+    parser.add_argument(
+        'device', help="Device to use : CPU/GPU", choices=['cpu', 'gpu'])
+    parser.add_argument('-n', '--normalize',
+                        help='Enable normalization', action='store_true')
+    # parser.add_argument('path', help='Path to the dataset', type=str)
+    parser.add_argument(
+        'calculation', help='Select the calculation method : Rings only (0) / Frank Wolfe only (1) / both (2) / none (3) ', type=int, choices=[0, 1, 2, 3])
+    parser.add_argument('labelNode', help='Labels for the nodes. Depends on the dataset file',
+                        nargs='?', type=str, default='label')
+    parser.add_argument('labelEdge', help='Labels for the edges. Depends on the dataset file',
+                        nargs='?', type=str, default='bond_type')
+    args = parser.parse_args()
+
+
+    Gs = []
+    y= []
+    for i in range(10):
+        graphSet,ySet = loadDataset("data/Acyclic/trainset_"+str(i)+".ds")
+
+        Gs = Gs + graphSet
+        y = y + ySet
+
+
+    Gs = Gs[:100]
+    y = y[:100]
+
+    # Configuraiton du modele
+    rings_andor_fw = dico_calc[args.calculation]
+    device = dico_device[args.device]
+    nb_epochs = 5
+    # Init dataset
+    # path_dataset = args.path
+
+
+    [train_graph, valid_graph, train_label, valid_label] = train_test_split(Gs, y, test_size=0.50,
+                                                                                    train_size=0.50, shuffle=True)
+
+
+
+
+
+
+
+
+
+    for g in Gs:
+        compute_extended_labels(g, label_node="label")
+
+    node_label = args.labelNode
+    edge_label = args.labelEdge
+    node_labels, nb_edge_labels = build_node_dictionnary(
+        Gs, node_label, edge_label)
+    nb_labels = len(node_labels)
+
+
+    model = RegressGedLayer(train_graph ,torch.tensor(train_label), 3,nb_labels, nb_edge_labels, node_labels, rings_andor_fw, normalize=args.normalize,
+                     node_label=node_label)
+
+
+
+
+
+
+    graph_idx = torch.arange(0, len(valid_graph), dtype=torch.int64)
+    dataSet = torch.utils.data.TensorDataset(torch.tensor(graph_idx), torch.tensor(valid_label))
+    trainloader = torch.utils.data.DataLoader(dataSet, batch_size=8, shuffle=True, drop_last=True,
+                                              num_workers=0)  # 128, len(couples_train)
+
+
+
+
+
+
+
+    criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    for epoch in range(1000):
+        for graphs_idx, temp in trainloader:
+            optimizer.zero_grad()
+            ged_pred = torch.zeros(len(graphs_idx))
+            pred_val = torch.zeros(len(graphs_idx))
+             # forward
+
+
+
+
+
+             
+            with torch.set_grad_enabled(True):
+                for i in range(len(graphs_idx)):
+                    pred_val[i] =  model(valid_graph[graphs_idx[i]])
+
+            loss  = criterion(pred_val ,temp)
+            loss.backward()
+
+            optimizer.step()
+            print(loss)
+
+
+
+
+
+
+
+
+
+    # # Getting the GPU status :
+    # if(args.verbosity):
+    #     GPUtil.showUtilization()
+    # InsDel, nodeSub, edgeSub, loss_valid_plt, loss_train_plt = GEDclassification(
+    #     model, Gs, nb_epochs, device, y, rings_andor_fw, verbosity=args.verbosity)
+
+
+
+
+
+
+    # if(args.verbosity):
+    #     print(loss_train_plt, loss_valid_plt)
+    #     visualize(InsDel, nb_epochs, nodeSub, edgeSub, loss_valid_plt)
+    # # We save the losses into pickle files
+    # save_data(loss_valid_plt, loss_train_plt, InsDel, edgeSub,
+    #           nodeSub, rings_andor_fw)
---
+++ model_regression.py
@@ -0,0 +1,173 @@
+from deepged.model import GedLayer
+import torch
+import torch.nn as nn
+
+
+
+
+class RegressGedLayer(nn.Module):
+    def __init__(self, Gs_train ,y_train, k , nb_labels, nb_edge_labels, dict_nodes, rings_andor_fw='sans_rings_sans_fw',
+                 normalize=False, node_label="label",verbose=True):
+        super(RegressGedLayer, self).__init__()
+
+
+        self.gedLayer  =   GedLayer(nb_labels, nb_edge_labels, dict_nodes, rings_andor_fw, normalize=normalize,
+                         node_label=node_label)
+
+
+        self.knnLayer = KnnRegressFromGED(y_train, k)
+        self.y_train = y_train
+        self.Gs_train = Gs_train
+
+
+    def forward(self , graph ):
+        ged_Array = torch.zeros(len(self.Gs_train))
+        for i in range(len(self.Gs_train)):
+
+            ged =  self.gedLayer((graph,self.Gs_train[i] ))
+            ged_Array[i] = ged
+
+        # print(torch.tensor(ged_Array))
+        # return self.knnLayer(ged_Array)
+        return ged_Array
+
+
+
+
+
+
+
+
+class KnnRegressFromGED(nn.Module):
+    def __init__(self, y, k, nb_test=5, device=torch.device("cpu"), weights='uniform'):
+        super(KnnRegressFromGED, self).__init__()
+
+        self.y = y
+        self.k = k
+        self.nb_test = nb_test
+        self.device = device
+        self.weights = weights
+        # self.coef_dist = nn.Parameter(torch.tensor(
+        #     0.85, requires_grad=True, device=self.device))
+
+    def normalize_val(self, val):
+        n = val.shape[0]
+        m = val.shape[1]
+        alpha = self.coef_dist*self.coef_dist
+        ones_m = torch.ones((1, m), device=val.device)
+
+        min = val[:, 0].view(n, 1)@ones_m
+        v2 = val-min
+        normal = (1.0/(alpha*torch.std(v2, dim=1))).view(n, 1)@ones_m
+        print('alpha=', alpha)
+
+        return v2*normal
+
+    # def forward(self, ged):
+    #
+    #     # ged is supposed to be a 1D array of size (train_size-nb_test X nb_test)
+    #     # train_size = 10
+    #     # x = ged.view((self.nb_test, train_size-self.nb_test))
+    #     get = torch.nn.functional.normalize(ged , dim=0)
+    #
+    #
+    #     val, ind = torch.topk(ged, self.k, dim=0, largest=False)
+    #     val_y, ind_y = torch.topk(self.y, self.k, dim=0, largest=False)
+    #
+    #
+    #     val_y_inv, ind_y_inv = torch.topk(self.y, len(self.y) - self.k, dim=0, largest=True)
+    #
+    #
+    #
+    #     sum = torch.sum(ged[ind_y_inv] - 1/(len(ind_y_inv)) ) + torch.sum(ged[ind_y])
+    #     # if self.weights == 'uniform':
+    #     # print(val)
+    #     # print(ind)
+    #
+    #     return torch.sum(self.y[ind], 0)/self.k, sum
+
+
+
+    def forward(self, ged):
+
+
+        val, ind = torch.topk(ged, self.k, dim=0, largest=False)
+
+        return torch.sum(self.y[ind], 0)/self.k
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #        sim=torch.exp(-alpha*val)
+#         val = self.normalize_val(val)
+#         sim = 1.0/(val+1.0)
+# #        m=torch.nn.Softmax(dim=1)
+# #        sim=m(-val)
+# #        print('y=',self.y[ind])
+# #        print('val=',val)
+# #        print('sim=',sim)
+# #        print('res=',torch.sum(sim*self.y[ind],1)/torch.sum(sim,1))
+#         return torch.sum(sim*self.y[ind], 1)/torch.sum(sim, 1)
+
+
+
+class RegressFromGED(nn.Module):
+    def __init__(self, y, normalize=None, eps=10**(-6), nb_test=5):
+        super(RegressFromGED, self).__init__()
+
+        self.y = y
+        self.normalize = normalize
+        self.eps = eps
+        self.nb_test = nb_test
+        self.device = torch.device("cuda:0")
+        self.coef_dist = nn.Parameter(torch.tensor(
+            1.0, requires_grad=True, device=self.device))
+        self.regu = nn.Parameter(torch.tensor(
+            1.0, requires_grad=True, device=self.device))
+
+    def forward(self, ged):
+        # Warning, the ged are supposed normalized between 0 and 1.
+        x = torch.tensor(ged.size(), dtype=torch.float)
+        nb_graph = ((1+torch.sqrt(1+8*x))/2.0).int()
+        D = torch.zeros(nb_graph, nb_graph, device=ged.device)
+        indices = torch.triu_indices(
+            nb_graph.item(), nb_graph.item(), offset=1, device=ged.device)
+        D[indices[0], indices[1]] = ged
+        D = D+D.T
+        alpha = self.coef_dist*self.coef_dist
+        regu = self.regu*self.regu
+
+        if self.normalize == 'exp':
+            K = torch.exp(-alpha*D/D.mean(dim=[0, 1]))
+        else:
+            K = 1-.5*alpha*D
+        train_size = K.shape[0]-self.nb_test
+        K_train = K[0:train_size, 0:train_size]
+        K_test = K[0:train_size, train_size:]
+       # print('train_size=',train_size,', nb tests: ',self.nb_test)
+       # print('K=',K)
+        #print('K train : ',K_train.size())
+        #print('K test : ',K_test.size())
+        # print('y=',y.size())
+        #print('y train : ',y[0:train_size])
+        # print('D=',D)
+#        print('K=',K)
+#        U,mu,V=torch.svd(K_train,some=True)
+# #        if (mu>0.0).all():
+#         return self.y[0:train_size].T@torch.inverse(K_train+regu*torch.eye(train_size, device=ged.device))@K_test
+
+        keep = (mu > self.limit).int()
+        print('mu=', mu)
+        print('keep=', keep)
+        y_pred = ((keep*(1/(mu+self.eps)))*((V.T@y)*(V.T@K))).sum(dim=1)
+        return y_pred
