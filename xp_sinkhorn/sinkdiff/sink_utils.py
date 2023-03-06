4ee20d6700e99a73d76c574ff72d19ead2ae437f
Gauzere Benoit <benoit.gauzere@insa-rouen.fr>
2023-03-06 16:54:47 +0100

    xp calcul ged ok

--- xp_sinkhorn/sinkdiff/sink_utils.py
+++ xp_sinkhorn/sinkdiff/sink_utils.py
@@ -1,19 +1,35 @@
-import numpy as np
+"""
+Utilitaries for sinkhorn
+
+We assume that all matrices are torch matrices
+"""
+import torch
+
+
+def from_cost_to_similarity(C):
+    M = torch.max(C)+1.0
+    S = 2*M*torch.ones((C.shape[0], C.shape[1]))
+    S[-1, :] = M
+    S[:, -1] = M
+    return simplify_matrix(S-C)
 
 
 def cost_to_sim(C):
     """
     Transforms a cost matrix to a similarity matrix
     """
+    n, m = C.shape
+    c = torch.max(C)+1.0
+    matrix_sim = 2.0*c*torch.ones((n, m))
+    matrix_sim[-1, :] = c
+    matrix_sim[:, -1] = c
+    return simplify_matrix(matrix_sim - C)
 
 
 def sim_to_cost(S):
     """
     """
-    matrix_optim = 2.0*c*np.ones((n+1, m+1)) - similarity
-    matrix_optim[-1, 0:m] -= c
-    matrix_optim[0:n, -1] -= c
-    matrix_optim[-1, -1] = 0.0
+    pass
 
 
 def simplify_matrix(S):
@@ -23,8 +39,8 @@ def simplify_matrix(S):
     correspond au début de la page 5 d'ICPR  (simplifing the matrix)
     """
     def compute_threshold(S, n, m):
-        ones_n = np.ones((n, 1))
-        ones_m = np.ones((1, m))
+        ones_n = torch.ones((n, 1))
+        ones_m = torch.ones((1, m))
 
         # chaque elt i,j a pour seuil S[n+1,j] + S[i,m+1]
         C = ones_n@(S[-1, 0:m].reshape(1, m))
@@ -38,8 +54,9 @@ def simplify_matrix(S):
     C = compute_threshold(S, n, m)
 
     eps = 1e-4  # the low value
-    S2 = S.copy()
+    S2 = S.clone()
     # mets eps si S[i,j] < C pour les substitution
-    S2[0:n, 0:m] = np.where(S[0:n, 0:m] < C, eps*np.ones((n, m)), S[0:n, 0:m])
+    S2[0:n, 0:m] = torch.where(
+        S[0:n, 0:m] < C, eps*torch.ones((n, m)), S[0:n, 0:m])
 
     return S2
--- xp_sinkhorn/xp_ged.py
+++ xp_sinkhorn/xp_ged.py
@@ -1,24 +1,66 @@
+import random
+import sys
 from greycdata.loaders import load_acyclic, load_MAO
 import librariesImport
 import gedlibpy
 from pyged.ged import GED
 from pyged.costfunctions import ConstantCostFunction
 from pyged.solvers import SolverLSAP, SolverLSAPE, SolverSinkhorn
+import pickle
+from tqdm import tqdm
+import networkx as nx
+
+
+def run_xp(graphs, solvers):
+    """Parameters
+    ----------
+    graphs : List of networkx Graphs
+    solvers : list[Solver]
+        List of solvers to use to compute ged
+
+    Returns
+    -----------
+    geds : dict[str,list[list[float]]]
+        The list of computed geds for each solver in solvers
+
+    """
+    def getname(x): return x.__class__.__name__
+    cf = ConstantCostFunction(1, 3, 1, 3, label_to_compare="atom_symbol")
+    geds = {getname(solver): [] for solver in solvers}
+    for g1 in tqdm(graphs):
+        for g2 in graphs:
+            for solver in solvers:
+                ged = GED(cf, solver=solver)
+                distance, rho, varrho = ged.ged(g1, g2)
+                # print(f"{solver.__class__.__name__} : {distance}")
+                geds[getname(solver)].append(distance)
+
+    return geds
+
+
+def shuffle_nodes(graphs):
+    shuffled_graphs = []
+    for graph in graphs:
+        node_mapping = dict(zip(graph.nodes(), sorted(
+            graph.nodes(), key=lambda k: random.random())))
+        shuffled_graph = nx.relabel_nodes(graph, node_mapping)
+        shuffled_graphs.append(shuffled_graph)
+    return shuffled_graphs
 
 
 def main():
-
     graphs, properties = load_MAO()
+    # shuffle node order
+    shuffled_graphs = shuffle_nodes(graphs)
 
-    print(f'Number of graphs: {len(graphs)}')
-    print(f'Number of features: {graphs[0].nodes(data=True)}')
-    cf = ConstantCostFunction(1, 3, 1, 3)
+    print("Description du dataset;")
+    print(f'Number of graphs: {len(shuffled_graphs)}')
+    print(f'Number of features: {shuffled_graphs[0].nodes(data=True)}')
+    print("-"*80)
     solvers = [SolverLSAP(), SolverLSAPE(), SolverSinkhorn()]
-    for solver in solvers:
-        print(solver)
-        ged = GED(cf, solver=solver)
-        distance, rho, varrho = ged.ged(graphs[44], graphs[1])
-        print(distance)
+    geds = run_xp(shuffled_graphs, solvers)
+    with open("results_ged", 'wb') as f:
+        pickle.dump(geds, f)
 
 
 if __name__ == '__main__':
